a9b24336e7bab9125aa44646d747a1885a6c89e6
github username <github email address>
2017-04-14 15:15:35 -0400

    added controllers and views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  before_action :set_user , only:[:edit,:show,:update]
+  before_action :logged_in?, only:[:edit,:show,:update]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:height,:happiness,:nausea,:tickets,:admin,:password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-
-
+  resources :users
+  resources :rides
+  resources :attractions
+  # resources :sessions
+  root to:'sessions#index'
+  get '/login' , to: 'sessions#new' , as: :login
+  post '/sessions' , to: 'sessions#create', as: :create_session
+  delete '/logout', to: 'sessions#destroy' , as: :destroy_session
 end
